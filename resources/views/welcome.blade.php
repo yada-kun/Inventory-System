e0e5b43b4ca420d77a3bd4343dc532027e9bb4a6
@Emvir1 <riveraemvir@gmail.com>
2023-04-26 16:35:57 +0800

    modify dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,14 @@
 @extends('layouts.master')
 @section('content')
 
-<h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, adipisci in esse veritatis saepe non, nam distinctio deserunt libero a mollitia magni similique natus aliquam eligendi ullam sunt vel deleniti ipsum? Officiis porro, dolor fugit id repudiandae debitis! Corporis, minus? Deleniti voluptatibus aliquid, obcaecati corporis nesciunt reiciendis beatae! Suscipit, facere provident? Voluptate, animi consectetur? Iusto deserunt aut quas pariatur rerum vitae veniam, sequi porro id debitis, quidem aspernatur eius a? At veritatis quod suscipit eaque nam. Vero sunt perferendis unde vitae omnis earum maiores iusto ipsum similique amet, ut tempore quo quae iste quod harum animi ex, corrupti possimus. Vitae.</h3>
+<h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, adipisci in esse veritatis saepe non, nam
+    distinctio deserunt libero a mollitia magni similique natus aliquam eligendi ullam sunt vel deleniti ipsum? Officiis
+    porro, dolor fugit id repudiandae debitis! Corporis, minus? Deleniti voluptatibus aliquid, obcaecati corporis
+    nesciunt reiciendis beatae! Suscipit, facere provident? Voluptate, animi consectetur? Iusto deserunt aut quas
+    pariatur rerum vitae veniam, sequi porro id debitis, quidem aspernatur eius a? At veritatis quod suscipit eaque nam.
+    Vero sunt perferendis unde vitae omnis earum maiores iusto ipsum similique amet, ut tempore quo quae iste quod harum
+    animi ex, corrupti possimus. Vitae.</h3>
 
-<h3>kobe</h3>
+<h3>kobe putragis</h3>
 
 @endsection
